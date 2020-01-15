22183684ab1da2d1f1692dbb5bf4f59d84873918
Brian Hoffman <bhoffco@gmail.com>
2020-01-14 19:51:21 -0500

    first commit

---
+++ 3LTB - live/3LTB - live/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3LTB___live.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
